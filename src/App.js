93461c1f3503fd9476355720640430c19eebaa9f
Marco Galizzi <marcogalizzi89@gmail.com>
2018-11-19 23:15:52 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './containers/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      boilerplate
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+          </Switch>
+        </Router>
       </div>
     );
   }
